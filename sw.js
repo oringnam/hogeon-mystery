c0510ae28f04921bd5b0c2a9bb77a001ef9a78f9
oringnam <oringnam@gmail.com>
2026-02-15 00:08:56 +0900

    v4.1 add ending illustrations + cache bump

--- sw.js
+++ sw.js
@@ -1,10 +1,20 @@
 // Service Worker - 캐싱으로 빠른 로딩
-const CACHE_NAME = 'hogeon-mystery-v43';
+const CACHE_NAME = 'hogeon-mystery-v44';
 const urlsToCache = [
   '/',
   '/index.html',
   '/games/hogeon/index.html',
   '/games/hogeon/story.json',
+  '/games/hogeon/images/prologue.png',
+  '/games/hogeon/images/ending_lotto.png',
+  '/games/hogeon/images/ending_ship.png',
+  '/games/hogeon/images/ending_hospital.png',
+  '/games/hogeon/images/ending_witness.png',
+  '/games/hogeon/images/ending_isekai.png',
+  '/games/hogeon/images/ending_spy.png',
+  '/games/hogeon/images/ending_religion.png',
+  '/games/hogeon/images/ending_business.png',
+  '/games/hogeon/images/ending_overseas.png',
   '/sounds/dark-fog-bgm.mp3',
   '/sounds/investigation-old.mp3',
   '/sounds/mystery-new.mp3',
