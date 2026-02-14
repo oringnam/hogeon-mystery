089bea92099c19e8bbb3f22672d2b70879e522c3
oringnam <oringnam@gmail.com>
2026-02-14 12:28:03 +0900

    Add Service Worker for caching - 2번째 방문부터 즉시 로드

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Service Worker - 캐싱으로 빠른 로딩
+const CACHE_NAME = 'hogeon-mystery-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/games/hogeon/index.html',
+  '/games/hogeon/story.json',
+  'https://unpkg.com/inkjs@2.3.0/dist/ink.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
